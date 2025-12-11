1b0c9f92dbbf8f53afa1c94c5cbfdb71677b01c3
scalgary <sandrine.lebon@ipsos.com>
2025-12-10 17:14:25 -0700

    WIP: work on CSS

--- my_app/routes/messages.py
+++ my_app/routes/messages.py
@@ -97,7 +97,7 @@ def community_pages(
     messages = message_service.get_all_messages_with_comments()
     
     # Render template
-    return templates.TemplateResponse("community.html", {
+    return templates.TemplateResponse("test.html", {
         "request": request,
         "user": user,
         "memberships": memberships,
